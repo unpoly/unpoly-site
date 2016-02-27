68775682e50c42034a42016541d70c46c4d7572b
Henning Koch <henning.koch@makandra.de>
2016-02-27 20:22:10 +0100

    Fix guide pages that start with a dot

--- lib/unpoly/guide/util.rb
+++ lib/unpoly/guide/util.rb
@@ -5,6 +5,9 @@ module Unpoly
 
         def slugify(string)
           guide_id = string.dup
+          # Even though we're OK with these characters in the middle of
+          # a slug, we don't want them at the beginning
+          guide_id.gsub!(/^[\:\-\.]+/, '-')
           # Colons (used in up:events) are valid URL segments, as used e.g. by
           # Wikipedia: https://en.wikipedia.org/wiki/Template:Welcome
           guide_id.gsub!(/[^A-Za-z0-9\:\-\.]/, '-')
