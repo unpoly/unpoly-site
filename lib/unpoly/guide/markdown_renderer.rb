4722e1941001a1e94a3df274eb32e26f8cbe0cbd
Henning Koch <henning.koch@makandra.de>
2022-10-25 22:12:10 +0200

    Parse admonition blockquotes; Refine many styles

--- lib/unpoly/guide/markdown_renderer.rb
+++ lib/unpoly/guide/markdown_renderer.rb
@@ -11,6 +11,7 @@ module Unpoly
         @strip_links = options.fetch(:strip_links, false)
         @pictures = options.fetch(:pictures, true)
         @fix_relative_image_paths = options.fetch(:fix_relative_image_paths, true)
+        @admonitions = options.fetch(:admonitions, true)
         @link_current_path = options.fetch(:link_current_path, false)
         @current_path = options.fetch(:current_path) if link_current_path
       end
@@ -21,6 +22,7 @@ module Unpoly
       attr_reader :fix_relative_image_paths
       attr_reader :link_current_path
       attr_reader :current_path
+      attr_reader :admonitions
 
       def to_html(text)
         doc = Kramdown::Document.new(text,
@@ -62,7 +64,13 @@ module Unpoly
           end
         end
 
-        nokogiri_doc.to_html
+        html = nokogiri_doc.to_html
+
+        if admonitions
+          html = parse_msdoc_admonitions(html)
+        end
+
+        html
       end
 
       def autolink_code_in_nokogiri_doc(nokogiri_doc, link_current_path: false)
@@ -85,6 +93,54 @@ module Unpoly
 
       def normalized_current_path
 
+      end
+
+      ADMONITION_ICONS = {
+        'WARNING' => 'exclamation-triangle',
+        'ATTENTION' => 'exclamation-triangle',
+        'CAUTION' => 'exclamation-triangle',
+        'NOTE' => 'exclamation-circle',
+        'IMPORTANT' => 'exclamation-circle',
+        'TIP' => 'eye',
+        'HINT' => 'eye',
+        'INFO' => 'info-circle',
+      }.freeze
+
+      ADMONITION_TYPES = ADMONITION_ICONS.keys.freeze
+
+      # The types of admonition boxes we support.
+      # Types on the same line get the same visual style, as e.g. "tip" and "hint"
+      # is really the same thing.
+      ADMONITION_TYPE_PATTERN = Regexp.new(ADMONITION_TYPES.join('|'), 'i')
+
+      ADMONITION_SIGNATURE_PATTERN = %r{
+        (#{ADMONITION_TYPE_PATTERN}) # type ($1)
+        (?:\ "([^\n"]*)")?           # optional title ($2)
+      }xi
+
+      RENDERED_MSDOC_ADMONITION_PATTERN = %r{
+        <blockquote>                           # opening <blockquote>
+        ((?:\s|<p>|<br>)*)                     # content start ($1)
+        \[!?#{ADMONITION_SIGNATURE_PATTERN}\]  # admonition type ($2) and optional title ($3)
+        (?:\s*</p>\s*<p>\s*)?                  # redundant paragraph if user puts a newline below the signature
+        ((?:<br>|\s)*)                         # signature/content separator to discard ($4)
+        # Don't match the end which is non-trivial for nested blockquotes
+      }xi
+
+      # Convert admonition markdown in the style of docs.microsoft.com to a
+      # BEM block <div class="admonition">.
+      def parse_msdoc_admonitions(html)
+        html.gsub(RENDERED_MSDOC_ADMONITION_PATTERN) do
+          content_start = $1
+          admonition_type = $2.upcase
+          admonition_title = $3.presence || admonition_type.titleize
+          admonition_icon = ADMONITION_ICONS.fetch(admonition_type)
+          <<~HTML.html_safe
+            <blockquote class='admonition -#{admonition_type.downcase}'>
+              <h4 class='admonition--title'><i class='fa fa-#{admonition_icon}'></i> #{admonition_title}</h4>
+              #{content_start}
+          HTML
+        end
       end
 
       def guide
