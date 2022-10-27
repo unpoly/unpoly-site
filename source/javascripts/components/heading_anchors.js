a4489025f72812f55dee2d5806b264da70ae3e8c
Henning Koch <henning.koch@makandra.de>
2022-10-27 12:09:57 +0200

    When hovering over a heading with an [id], show a '#' link with an anchor to that heading

---
+++ source/javascripts/components/heading_anchors.js
@@ -0,0 +1,6 @@
+up.compiler('h1[id], h2[id], h3[id], h4[id], h5[id]', function(heading) {
+  let hoverArea = up.element.createFromSelector('span.heading-anchor', { 'aria-hidden': true })
+  let href = '#' + heading.id
+  up.element.affix(hoverArea, 'a.heading-anchor--link', { text: '#', href })
+  heading.prepend(hoverArea)
+})
