7c459abf45e1232a20936cf52a35eb3fc3959e6c
Henning Koch <henning.koch@makandra.de>
2022-12-09 09:25:23 +0100

    CSS

--- source/javascripts/components/heading_anchors.js
+++ source/javascripts/components/heading_anchors.js
@@ -1,4 +1,4 @@
-up.compiler('h1[id], h2[id], h3[id], h4[id], h5[id]', function(heading) {
+up.compiler('h1[id], h2[id], h3[id], h4[id], h5[id], [anchor-link][id]', function(heading) {
   let hoverArea = up.element.createFromSelector('span.heading-anchor', { 'aria-hidden': true })
   let href = '#' + heading.id
   up.element.affix(hoverArea, 'a.heading-anchor--link', { text: '#', href })
