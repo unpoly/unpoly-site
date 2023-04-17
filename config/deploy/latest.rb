c015f6fc8c2360f1a43d896505dce97c2b26d486
Henning Koch <henning.koch@makandra.de>
2023-04-17 14:22:43 +0200

    Delete old deploy targets

--- config/deploy/latest.rb
+++
@@ -1,7 +0,0 @@
-set :deploy_to, "/var/www/unpoly.com/"
-set :branch, 'master'
-# set :middleman_options, %w[--environment=production]
-set :keep_releases, 10
-
-server 'app01-prod.makandra.makandra.de', user: 'deploy-unpoly_p', roles: %w(app)
-server 'app02-prod.makandra.makandra.de', user: 'deploy-unpoly_p', roles: %w(app)
